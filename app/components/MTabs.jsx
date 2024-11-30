d1e83ebc7397e2da68b229f24fbdfa7fc264cc22
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-11-30 10:22:28 +0530

    Add files via upload

--- app/components/MTabs.jsx
+++ app/components/MTabs.jsx
@@ -1,7 +1,6 @@
 'use client'
 
 import * as React from 'react';
-import { useState, useEffect } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
@@ -9,10 +8,6 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function MTabs() {
     const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
 
     function handleChange1(newValue) {
         setValue(newValue);
@@ -29,26 +24,29 @@ export default function MTabs() {
         },
     });
 
-    useEffect(() => {
-        // elementInViewport("Divide1", 0);
-        // elementInViewport("Divide2", 1);
-        //{ elementInViewport("Divide3", 2) }
-        // elementInViewport("Divide4", 3);
-        // elementInViewport("Divide5", 4);
-        //alert("hello")
-    }, [])
+    setInterval((e) => { elementInViewport("Divide1", 0) }, 500);
+    setInterval((e) => { elementInViewport("Divide2", 1) }, 500);
+    setInterval((e) => { elementInViewport("Divide3", 2) }, 500);
+    setInterval((e) => { elementInViewport("Divide4", 3) }, 500);
+    setInterval((e) => { elementInViewport("Divide5", 4) }, 500);
 
-    function elementInViewport(gotit, place) {
+    const elementInViewport = (gotit, place) => {
         var myElement = document.getElementById(gotit);
-        var bounding = myElement.getBoundingClientRect();
-        if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight))) { handleChange1(place) }
+        const rect = myElement.getBoundingClientRect();
+
+        const isInViewport = rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+        { isInViewport && handleChange1(place) }
     }
 
     const Move = (e) => {
+
         const yOffset = -50;
         const element = document.getElementById(e);
         const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
-
         window.scrollTo({ top: y, behavior: 'smooth' });
     }
 
@@ -56,16 +54,16 @@ export default function MTabs() {
         <Box sx={{ width: '100%' }}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <ThemeProvider theme={theme}>
-                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab  sx={{ color: "#FFFFFF" }}  className='text-white normal-case' label="Home"
+                    <Tabs value={value} aria-label="basic tabs example">
+                        <Tab sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="Home"
                             onClick={(e) => { Move("homemove") }} />
-                        <Tab sx={{ color: "#FFFFFF" }}  className='text-white normal-case' label="About me"
+                        <Tab sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="About me"
                             onClick={(e) => { Move("aboutmove") }} />
-                        <Tab  sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="Skills"
+                        <Tab sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="Skills"
                             onClick={(e) => { Move("skillsmove") }} />
-                        <Tab  sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="Projects"
+                        <Tab sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="Projects"
                             onClick={(e) => { Move("projectsmove") }} />
-                        <Tab sx={{ color: "#FFFFFF" }}  className='text-white normal-case' label="Contact"
+                        <Tab sx={{ color: "#FFFFFF" }} className='text-white normal-case' label="Contact"
                             onClick={(e) => { Move("contactmove") }} />
                     </Tabs>
                 </ThemeProvider>
