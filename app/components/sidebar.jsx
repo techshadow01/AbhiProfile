241eaeae96be2f2db3df5418458e6f8385400280
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-01 18:46:22 +0530

    Add files via upload

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -9,11 +9,11 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const sidebar = () => {
     return (
-        <div className='w-[100px] h-[full] flex flex-col items-start justify-center gap-6 text-xl'>
-            <a href='https://github.com/techshadow01' target="_blank"> <motion.div whileHover={{ scale: 1.2 }}><GitHubIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }}/></motion.div></a>
-            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><LinkedInIcon className='text-white size-10 cursor-pointer hover:text-pink-600'sx={{ fontSize: 40 }} /></motion.div></a>
-            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><XIcon className='text-white size-10 cursor-pointer hover:text-pink-600'sx={{ fontSize: 40 }} /></motion.div></a>
-            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><MailOutlineIcon className='text-white size-10 cursor-pointer hover:text-pink-600'sx={{ fontSize: 40 }} /></motion.div></a>
+        <div className='w-[100px] h-[full] flex flex-col items-start justify-center gap-6 text-xl max-sm:hidden'>
+            <a href='https://github.com/techshadow01' target="_blank"> <motion.div whileHover={{ scale: 1.2 }}><GitHubIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
+            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><LinkedInIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
+            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><XIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
+            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><MailOutlineIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
         </div>
     )
 }
