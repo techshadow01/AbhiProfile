0a563d53ee4604946276907a4ae6accaa7d3ec88
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-04 19:04:38 +0530

    Add files via upload

---
+++ app/ui/homebgs.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './homebg.css'
+
+const homebgs = () => {
+    return (
+        <div class="area" >
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div >
+    )
+}
+
+export default homebgs
