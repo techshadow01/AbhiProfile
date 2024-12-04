f53b69dea37a6b8c349a5fc0b03db18a5667e2a6
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-04 18:54:02 +0530

    Add files via upload

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -11,9 +11,9 @@ const sidebar = () => {
     return (
         <div className='w-[100px] h-[full] flex flex-col items-start justify-center gap-6 text-xl max-sm:hidden'>
             <a href='https://github.com/techshadow01' target="_blank"> <motion.div whileHover={{ scale: 1.2 }}><GitHubIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
-            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><LinkedInIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
-            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><XIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
-            <a href='https://github.com/techshadow01' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><MailOutlineIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
+            <a href='https://www.linkedin.com/in/abhishek-jangid-079b9726a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><LinkedInIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
+            <a href='https://x.com/AbhishekJa94069?t=T_vVqs7Bbcj1GjLx0tei4g&s=08' target="_blank"><motion.div whileHover={{ scale: 1.2 }}><XIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
+            <a href="mailto:shadow459434@gmail.com"> <motion.div whileHover={{ scale: 1.2 }}><MailOutlineIcon className='text-white size-10 cursor-pointer hover:text-pink-600' sx={{ fontSize: 40 }} /></motion.div></a>
         </div>
     )
 }
