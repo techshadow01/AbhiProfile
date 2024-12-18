e35bded397189554db63eafbe05a5f4831772879
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-18 21:25:34 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import localFont from "next/font/local";
  import Navbar from './components/navbar'
-import About from './about'
- import Skills from './skills'
- import Projects from './projects'
- import Contact from './contact'
+import About from './about/page.js'
+import Skills from './skills/page.js'
+import Projects from './projects/page.js'
+import Contact from './contact/page.js'
 
 import "./globals.css";
 
