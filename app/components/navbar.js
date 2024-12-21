e13ce7ae5a613dbf6008386befd644778c045af4
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-21 22:06:29 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,49 +1,25 @@
 'use client'
 
 import React from 'react'
-import { useState, useEffect } from 'react';
 import Tabs from './MTabs'
 import './components.css'
 import Mnavbar from './mnavbar'
 
-
 const navbar = () => {
-    const [prevScrollPos, setPrevScrollPos] = useState(0);
-    const [visible, setVisible] = useState(true)
-
-    const handleScroll = () => {
-        const currentScrollPos = window.scrollY
-
-        if (currentScrollPos > prevScrollPos) {
-            setVisible(false)
-        } else {
-            setVisible(true)
-        }
-
-        setPrevScrollPos(currentScrollPos)
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-
-        return () => window.removeEventListener('scroll', handleScroll)
-    })
-
-
     return (
-        <div className={`fixed  z-20 w-[100vw] h-[50px] trans flex items-center justify-center border-pink-600 max-sm:border max-sm:rounded-full max-sm:w-[50vw] max-sm:ml-[25vw]  max-sm:bg-black duration-700 ${visible ? 'max-sm:top-5' : 'max-sm:-top-14'} `}>
-            <div className={`w-[90vw] text-white flex items-center justify-between max-sm:hidden  `}>
-                <a href="./"><div className='flex items-center justify-center gap-2 text-2xl max-sm:hidden'>
-                    <div className='text-pink-600 font-bold '>Abhishek</div>
+        <div className='fixed top-0 z-20 w-[100vw] h-[50px] trans flex items-center justify-center'>
+            <div className='w-[90vw] text-white flex items-center justify-between'>
+                <a href="./"><div className='flex items-center justify-center gap-2 text-2xl'>
+                    <div className='text-pink-600 font-bold'>Abhishek</div>
                     <div>Jangid</div>
                 </div></a>
                 <div className='flex items-center justify-center max-sm:hidden'>
+
                     <Tabs className="max-sm:hidden" />
                 </div>
-            </div>
-
-            <div className={`w-full  sm:hidden `}>
-                <Mnavbar />
+                <div className='sm:hidden'>
+                    <Mnavbar />
+                </div>
             </div>
         </div >
     )
