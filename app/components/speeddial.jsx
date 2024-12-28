5bd3f2f87d3b258fb36bf0cdbb5cc2b164fa913c
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-28 16:14:00 +0530

    Add files via upload

---
+++ app/components/speeddial.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import XIcon from '@mui/icons-material/X';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
+const actions = [
+    { icon: <a href='https://github.com/techshadow01' target="_blank"> <GitHubIcon className='text-black' /></a>, name: 'Github' },
+    { icon: <a href='https://www.linkedin.com/in/abhishek-jangid-079b9726a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target="_blank"><LinkedInIcon className='text-black' /></a>, name: 'Linkedin' },
+    { icon: <a href='https://x.com/AbhishekJa94069?t=T_vVqs7Bbcj1GjLx0tei4g&s=08' target="_blank"><XIcon className='text-black' /></a>, name: 'Twitter' },
+    { icon: <a href="mailto:shadow459434@gmail.com"> <MailOutlineIcon className='text-black' /></a>, name: 'Mail' },
+];
+
+function BasicSpeedDial() {
+    return (
+        <Box className="h-[320px] flex-grow fixed z-20 bottom-2 right-8 sm:hidden">
+            <SpeedDial
+                ariaLabel="SpeedDial basic example"
+
+                icon={<SpeedDialIcon className='bg-slate-600 size-14 rounded-full flex items-center justify-center' />}
+            >
+                {actions.map((action) => (
+                    <SpeedDialAction
+
+                        key={action.name}
+                        icon={action.icon}
+                        tooltipTitle={action.name}
+                    />
+                ))}
+            </SpeedDial>
+        </Box>
+    );
+}
+
+const speeddial = () => {
+    return (
+        <div><BasicSpeedDial /></div>
+    )
+}
+
+export default speeddial;
