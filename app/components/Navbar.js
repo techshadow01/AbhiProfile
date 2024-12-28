3973eb99ffadc7677d6d4ecc32ceb3bf881707f4
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-28 16:33:48 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,11 +3,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import Tabs from './MTabs'
-import './components.css'
 import Mnavbar from './mnavbar'
 
 
-const Navbar = () => {
+const navbar = () => {
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true)
 
@@ -31,10 +30,10 @@ const Navbar = () => {
 
 
     return (
-      <div className={`fixed  z-20 w-[100vw] h-[50px] trans flex items-center justify-center border-pink-600 max-sm:border max-sm:rounded-full max-sm:w-[60vw] max-sm:ml-[20vw]  max-sm:bg-black duration-700 ${visible ? 'max-sm:top-5' : 'max-sm:-top-14'} `}>
+        <div className={`fixed  z-20 w-[100vw] h-[50px] trans flex items-center justify-center border-slate-600 max-sm:border max-sm:rounded-full max-sm:w-[60vw] max-sm:ml-[20vw]  max-sm:bg-white max-sm:bg-opacity-10 duration-700 ${visible ? 'max-sm:top-5' : 'max-sm:-top-14'} `}>
             <div className={`w-[90vw] text-white flex items-center justify-between max-sm:hidden  `}>
                 <a href="./"><div className='flex items-center justify-center gap-2 text-2xl max-sm:hidden'>
-                    <div className='text-pink-600 font-bold '>Abhishek</div>
+                    <div className='text-white bg-pink-600 rounded-lg px-2 font-bold '>Abhishek</div>
                     <div>Jangid</div>
                 </div></a>
                 <div className='flex items-center justify-center max-sm:hidden'>
@@ -49,4 +48,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default navbar
