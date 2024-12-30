747fe93d3cf85960290f62a251ac7ffc72200f38
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2024-12-30 21:00:19 +0530

    Update Modal1.jsx

--- app/projects/Modal1.jsx
+++ app/projects/Modal1.jsx
@@ -11,6 +11,8 @@ import Alice from './assets/Alice.png'
 import Spotify from './assets/spotify.png'
 import Github from './assets/github.svg'
 import { motion } from "motion/react"
+import Zoom from '@mui/material/Zoom';
+import Share from './assets/share.svg'
 
 const Modal1 = (props) => {
    const [open, setOpen] = React.useState(false);
@@ -24,47 +26,49 @@ const Modal1 = (props) => {
                 open={open}
                 onClose={handleClose}
             >
-                <Box className='absolute z-20 w-[90%] top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-xl overflow-hidden'>
-                    <button className='absolute text-2xl text-white
+                <Zoom in={open} className='top-[calc(50%-250px)] left-[5%] absolute z-20 w-[90%]  transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-xl overflow-hidden'>
+                    <Box className='transform -translate-x-1/2 -translate-y-1/2 '>
+                        <button className='absolute text-2xl text-white
                      z-40 right-5 top-5 font-bold' onClick={handleClose}>
-                        <Image src={Cross} alt="" width={10} />
-                    </button>
+                            <Image src={Cross} alt="" width={10} />
+                        </button>
 
-                    <div className='w-screen '>
-                        <Image className='w-[90%] h-[25%] object-contain' src={props.item.img} alt="" width={400} />
-                    </div>
-
-                    <motion.div
-                        className='flex flex-col gap-4 p-2 '>
-                        <div className={`text-4xl font-bold ${props.item.text}`}>{props.item.title}</div>
-                        <div className=' text-black text-xl '>{props.item.desc}</div>
-                        <div className='flex gap-4 '>
-                            <motion.div >
-                                <a href={props.item.live} target='_blank'>
-                                    <button className=' border bg-slate-500  rounded-full min-w-[100px] h-10 normal-case text-white'>
-                                        Live
-                                    </button>
-                                </a>
-                            </motion.div>
-                            <a href={props.item.code} target='_blank'><motion.div>
-                                <button className=' bg-slate-500 text-white rounded-full w-10 h-10 normal-case flex items-center justify-center' >
-                                    <Image src={Github} alt="" width={65} />
-                                </button>
-                            </motion.div></a>
+                        <div className='w-screen '>
+                            <Image className='w-[90%] h-[25%] object-contain' src={props.item.img} alt="" width={300} />
                         </div>
 
-                        <div className={`text-2xl font-bold ${props.item.text}`}>Tech Used :</div>
+                        <motion.div
+                            className='flex flex-col gap-4 p-2 '>
+                            <div className={`text-4xl font-bold ${props.item.text}`}>{props.item.title}</div>
+                            <div className=' text-black text-xl '>{props.item.desc}</div>
+                            <div className='flex gap-4 '>
+                                <motion.div >
+                                    <a href={props.item.live} target='_blank'>
+                                        <button className=''>
+                                            <Image src={Share} alt="" width={35} />
+                                        </button>
+                                    </a>
+                                </motion.div>
+                                <a href={props.item.code} target='_blank'><motion.div>
+                                    <button className=' bg-slate-600 rounded-full' >
+                                        <Image src={Github} alt="" width={35} />
+                                    </button>
+                                </motion.div></a>
+                            </div>
 
-                        {<div className='flex gap-2 rounded-full '>
-                            {(props.item.tech).map((item1, index1) => {
-                                return <div key={index1} className='rounded-full text-xl px-2'>
-                                    <Image src={item1} alt="" width={30} />
-                                </div>
-                            })}
-                        </div>}
-                    </motion.div>
-                </Box>
-            </Modal>
+                            <div className={`text-2xl font-bold ${props.item.text}`}>Tech Used :</div>
+
+                            {<div className='flex gap-2 rounded-full '>
+                                {(props.item.tech).map((item1, index1) => {
+                                    return <div key={index1} className='rounded-full text-xl px-2'>
+                                        <Image src={item1} alt="" width={30} />
+                                    </div>
+                                })}
+                            </div>}
+                        </motion.div>
+                    </Box>
+                </Zoom>
+            </Modal >
         </div >
     );
 }
